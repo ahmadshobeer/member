b45a335b7b277983e812b9661981defe119cc73b
unknown <ahmad.shobeer@gmail.com>
2024-12-10 15:54:21 +0700

    login,dashboard,migration

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,19 +6,19 @@
 
         <ul class="sidebar-nav">
             <li class="sidebar-header">
-                Pages
+                Menu
             </li>
 
             <li class="sidebar-item active">
-                <a class="sidebar-link" href="index.html">
-      <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-    </a>
+                <a class="sidebar-link" href="{{ url('/') }}">
+                <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+            </a>
             </li>
 
-            <li class="sidebar-item">
+          {{--   <li class="sidebar-item">
                 <a class="sidebar-link" href="pages-profile.html">
-      <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
-    </a>
+                <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                </a>
             </li>
 
             <li class="sidebar-item">
@@ -100,7 +100,7 @@
                     <a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </nav>
 
@@ -116,14 +116,14 @@
                     <a class="nav-icon dropdown-toggle" href="#" id="alertsDropdown" data-bs-toggle="dropdown">
                         <div class="position-relative">
                             <i class="align-middle" data-feather="bell"></i>
-                            <span class="indicator">4</span>
+                            <span class="indicator">0</span>
                         </div>
                     </a>
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="alertsDropdown">
                         <div class="dropdown-menu-header">
-                            4 New Notifications
+                            0 New Notifications
                         </div>
-                        <div class="list-group">
+                        {{-- <div class="list-group">
                             <a href="#" class="list-group-item">
                                 <div class="row g-0 align-items-center">
                                     <div class="col-2">
@@ -148,36 +148,14 @@
                                     </div>
                                 </div>
                             </a>
-                            <a href="#" class="list-group-item">
-                                <div class="row g-0 align-items-center">
-                                    <div class="col-2">
-                                        <i class="text-primary" data-feather="home"></i>
-                                    </div>
-                                    <div class="col-10">
-                                        <div class="text-dark">Login from 192.186.1.8</div>
-                                        <div class="text-muted small mt-1">5h ago</div>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#" class="list-group-item">
-                                <div class="row g-0 align-items-center">
-                                    <div class="col-2">
-                                        <i class="text-success" data-feather="user-plus"></i>
-                                    </div>
-                                    <div class="col-10">
-                                        <div class="text-dark">New connection</div>
-                                        <div class="text-muted small mt-1">Christina accepted your request.</div>
-                                        <div class="text-muted small mt-1">14h ago</div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
+                           
+                        </div> --}}
                         <div class="dropdown-menu-footer">
                             <a href="#" class="text-muted">Show all notifications</a>
                         </div>
                     </div>
                 </li>
-                <li class="nav-item dropdown">
+                {{-- <li class="nav-item dropdown">
                     <a class="nav-icon dropdown-toggle" href="#" id="messagesDropdown" data-bs-toggle="dropdown">
                         <div class="position-relative">
                             <i class="align-middle" data-feather="message-square"></i>
@@ -243,20 +221,18 @@
                             <a href="#" class="text-muted">Show all messages</a>
                         </div>
                     </div>
-                </li>
+                </li> --}}
                 <li class="nav-item dropdown">
                     <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
         <i class="align-middle" data-feather="settings"></i>
       </a>
 
                     <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-        <img src="img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="Charles Hall" /> <span class="text-dark">Charles Hall</span>
-      </a>
+                        <img src="{{ asset('img/avatars/avatar.jpg') }}" class="avatar img-fluid rounded me-1" alt="User Image" /> <span class="text-dark">Charles </span>
+                    </a>
                     <div class="dropdown-menu dropdown-menu-end">
                         <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-                        <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
                         <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Log out</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('auth/login', [AuthController::class, 'index'])->name('login');
