a8ea0ed11cbdbcb634597174e952c87d2f08675d
unknown <ahmad.shobeer@gmail.com>
2024-12-11 15:17:32 +0700

    profile page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('auth/login', [AuthController::class, 'index'])->name('login');
+Route::get('user/profile', [ProfileController::class, 'index'])->name('user/profile');
